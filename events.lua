e93628824df802e0e94fc446a47478b5c69c8bc5
Mastoast <dgekec@gmail.com>
2026-04-21 02:22:34 +0200

    Fix music trigger

--- events.lua
+++ events.lua
@@ -10,6 +10,7 @@ events = {
 	{t=17,type="text", text="HIII, I'm feeling soo good right now, I'm totally ready!",char="greyjean"},
 
 	{t=21,x=70,y=70,delay=1500,score=80,type="fire"},
+	{t=22,track=2,tempo=164,speed=6,type="music"},
 	{t=24,type="text", text="A distress signal for a Fire? That's a job for me!",char="aguaman"},
 	{t=34,x=180,y=35,delay=1500,score=80,type="fire"},
 	{t=36,type="text", text="I can do okay too, but not Grey Jeans...",char="milkachu"},
@@ -19,7 +20,6 @@ events = {
 	{t=49,type="text", text="Don't count on me for that one. Just send Milkachu.",char="aguaman"},
 
 	{t=56,type="text", text="He's too scared, I can beat bad guys too!!",char="greyjean"},
-	{t=60,track=2,tempo=164,speed=6,type="music"},			
 	{t=69,x=25,y=35,delay=1500,score=580,type="unrest"},
 	{t=69,type="text", text="Talking to civilians? Hard pass...",char="milkachu"},
 
